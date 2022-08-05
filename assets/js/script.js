a7d3eb92d894285365cf0e232154c40dff992f26
Huy <drac0123451@gmail.com>
2022-08-05 12:25:29 -0700

    add js to dynamically create timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,52 @@
 var now = moment();
 var currentDayEl = document.getElementById('currentDay');
 currentDayEl.textContent = moment(now).format('dddd, MMMM do');
+var blockContainer = document.querySelector('.container')
+var timeArr = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
+var text = $('.textarea')
+
+var pageLoad = function() {
+
+}
+
+
+var createTimeBlocks = function() {
+
+    for (var i = 0; i < timeArr.length; i++) {
+        // create div row container of the blocks
+        var divEl = document.createElement('div');
+        divEl.setAttribute("class", "row time-block");
+        blockContainer.appendChild(divEl);
+
+        // create block for time
+        var timeEl = document.createElement('div');
+        timeEl.setAttribute("class", "col-md-1 border-top hour");
+        timeEl.textContent = timeArr[i];
+        divEl.appendChild(timeEl);
+
+        // create block for content
+        var contentEl = document.createElement('textarea');
+        contentEl.setAttribute("class", "col-md-10 border textarea time-block");
+        contentEl.setAttribute("data-time-id", i);
+        divEl.appendChild(contentEl);
+
+        // create block for button
+        var buttonEl = document.createElement('button');
+        buttonEl.setAttribute("class", "col-md-1 saveBtn");
+        divEl.appendChild(buttonEl);
+
+        // create icon
+        var iEl = document.createElement('i');
+        iEl.textContent = "Save";
+        buttonEl.appendChild(iEl);
+    }
+};
+
+
+
+window.onload=createTimeBlocks();
+
+function notify() {
+    alert( "clicked" );
+  }
+  $( ".textgroup" ).on( "click", notify );
